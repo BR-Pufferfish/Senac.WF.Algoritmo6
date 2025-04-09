9612feea7b6ab49ebb5ee2f11b508b80f4b250e4
Marcelo Ballin <puffersenac@gmail.com>
2025-04-08 21:46:23 -0300

    form com 2 groupbox e labels e configurado botao sair com funcao

---
+++ SolucaoAlgoritmo6/Algoritmo6/Form1.cs
@@ -0,0 +1,30 @@
+namespace Algoritmo6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
